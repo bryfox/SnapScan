54923d247eee72a8a1e0415ebd2fa538181e5b6a
Bryan Fox <bryan@bryfox.com>
2017-06-05 15:28:40 -0400

    Add new Xcode project/workspace

---
+++ SnapScan/SnapScan/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SnapScan
+//
+//  Created by Bryan Fox on 6/5/17.
+//  Copyright © 2017 Bryan Fox. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
